b70e0820c29b2522c484fbee4dbb65730fa6b286
Robert Petro <112111334+Neptuneson@users.noreply.github.com>
2024-09-03 12:12:48 -0400

    Delete MATH229 directory

--- MATH229/carbon.R
+++
@@ -1,4 +0,0 @@
-carbon = biomass_data$carbon
-HHV = biomass_data$HHV
-
-plot(carbon, HHV, main = "The Effect of the Amount of Cabon on the HHV", xlab = "Amount of Cabon (mol)", ylab = "HHV (kJ/mol)")
